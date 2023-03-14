f02a4a3929ae2fc002929ec98648fb234d9a5a8b
Jaehyung Son <thswogud02@gmail.com>
2023-03-14 16:45:57 +0900

    Update [백준]1018.체스판다시칠하기.py
    1018. done

--- 백준/[백준]1018.체스판다시칠하기.py
+++ 백준/[백준]1018.체스판다시칠하기.py
@@ -3,26 +3,27 @@ N, M = map(int, input().split())
 data = [list(input()) for _ in range(N)]
 
 
-min_count = 70
+result = []
 for i in range(N):
     for j in range(M):
         # i와 j를 8번 하나씩 옮길건데 범위가 넘어가면 안됨.
         # 범위가 넘어가는 경우가 있는 지 확인 먼저
         if i+7 < N and j+7 < M:
-            count = 0
-            for k in range(8):
-                for a in range(8):
-                    ci = i + k
-                    cj = j + a
-                    # 색깔 검사해야 함
-                    # 오른쪽과 아래의 색깔이 달라야 함
-                    # 일단은 범위 넘지 않도록 해야 함
-                    if ci + 1 < N and cj + 1 < M:
-                        if data[ci][cj] == data[ci+1][cj] and data[ci][cj] == data[ci][cj+1]:
-                            count += 1
-            if count < min_count:
-                min_count = count
+            draw1 = 0
+            draw2 = 0
+            for a in range(i, i+8):
+                for b in range(j, j+8):
+                    if (a + b) % 2 == 0:
+                        if data[a][b] != 'B':
+                            draw1 += 1
+                        if data[a][b] != 'W':
+                            draw2 += 1
+                    else:
+                        if data[a][b] != 'W':
+                            draw1 += 1
+                        if data[a][b] != 'B':
+                            draw2 += 1
+            result.append(draw1)
+            result.append(draw2)
 
-print(min_count)
-
-
+print(min(result))
