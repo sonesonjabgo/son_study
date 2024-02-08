2ea211965925ecae93d66eb56844d1698f3d6328
Jaehyung Son <thswogud02@gmail.com>
2024-02-08 16:47:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,22 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let num = Number(input[0]);
+
+let answer = 0;
+let flag = false;
+
+while (num >= 0) {
+  if (num == 0 || num % 5 == 0) {
+    answer += parseInt(num / 5);
+    console.log(answer);
+    flag = true;
+    break;
+  }
+  num -= 3;
+  answer += 1;
+}
+
+if (!flag) {
+  console.log(-1);
+}
